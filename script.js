bada344c8a03b242da9d03c668ddefb6a987bf61
Ctrl-Shift-N <havannghiep7798@gmail.com>
2023-12-22 22:11:52 +0700

    pull1

--- script.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const div = document.getElementById("movableDiv");
-document.addEventListener("pointermove", moveDiv);
-function moveDiv(event) {
-    div.style.left = event.clientX + "px";
-    div.style.top = event.clientY + "px";
-}
